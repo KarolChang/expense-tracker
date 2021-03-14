a91e13e500a88e110955ddf01066f43e1530a5c9
Karol <super000999888@gmail.com>
2021-03-14 16:18:11 +0800

    feat: add user model and register route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   return res.render('login')
@@ -11,7 +12,24 @@ router.get('/register', (req, res) => {
   return res.render('register')
 })
 
-// router.post('/register')
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  return User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('This email has been registered!')
+        res.render('register', { name, email, password, confirmPassword})
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+      }
+    })
+    .then(() => res.render('login', { email }))
+    .catch(err => console.log(err))
+})
 
 // router.get('/logout')
 
